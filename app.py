0c27c0e8277aaead0808e4c148a6a152f9c8720d
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-12-18 11:37:11 -0500

    added two Graph Components

--- app.py
+++ app.py
@@ -5,14 +5,20 @@ from dash import Dash, html , dcc
 app = Dash() 
 
 # COMPONENTS 
+
 dd1 = dcc.Dropdown(['New York', 'Los Angeles', 'Chicago'], 
                    placeholder="Select city...",
                    id="dd-city-sel",
                    multi=True)
 
+graph1 = dcc.Graph(id="histo1")
+graph2 = dcc.Graph(id="histo2")
+
 # add layout
 app.layout = [
-    dd1
+    dd1,
+    graph1,
+    graph2
 ]
 
 if __name__ == '__main__':
