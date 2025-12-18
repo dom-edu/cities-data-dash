a884e27495a51f5aa71c8e60e5c968a787966431
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-12-18 11:51:58 -0500

    Added Bar Chart Component

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import plotly.express as px
 from dash import Dash, html , dcc 
 
 # DATA 
@@ -12,19 +13,21 @@ app = Dash()
 
 # COMPONENTS 
 
+# drop down to select cities
 dd1 = dcc.Dropdown(cities_df.name, 
                    placeholder="Select city...",
                    id="dd-city-sel",
                    multi=True)
 
-graph1 = dcc.Graph(id="histo1")
-graph2 = dcc.Graph(id="histo2")
+bar_chart = px.bar(cities_df, x='name', y='pop') 
+
+# graph to hold bar chart
+graph1 = dcc.Graph(id="histo1", figure=bar_chart)
 
 # add layout
 app.layout = [
     dd1,
     graph1,
-    graph2
 ]
 
 if __name__ == '__main__':
